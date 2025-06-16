3b9813df0b04523b9ee8ce03bec9e5e80fe91786
Goodspeed0121 <64545654+Goodspeed0121@users.noreply.github.com>
2025-06-17 05:09:14 +0800

    Add files via upload

---
+++ app_sentiment/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppSentimentConfig(AppConfig):
+    name = 'app_sentiment'
---
+++ app_top_person_v2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppTopPersonV2Config(AppConfig):
+    name = 'app_top_person_v2'
